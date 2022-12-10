8e524d188343045488ab1c43f353f60ad1a1005f
Ohad Bruker <ohad.bruker@lemonade.com>
2022-12-11 00:53:36 +0200

    Implement (2) Development set preprocessing

--- ex2/app.py
+++ ex2/app.py
@@ -1,5 +1,28 @@
 import sys
 
+
+def read_articles_file(filename):
+    with open(dev_filename, "r") as f:
+        content = f.read()
+        lines = [l for l in content.split("\n") if l]
+        i = 0
+        while i < len(lines):
+            header = lines[i]
+            if header[0] != "<" or header[-1] != ">":
+                raise Exception("header line expected to start with '<' and end with '>'")
+            header = header[1:-1]
+            _, id, *classes = header.split()
+            article = lines[i + 1]
+            i += 2
+            yield id, classes, article
+
+
+def concat_articles(articles_data):
+    return "\n".join(
+        [article for _, _, article in articles_data]
+    )
+
+
 if __name__ == "__main__":
     args = sys.argv[1:]
     if len(args) != 4:
@@ -13,6 +36,10 @@ if __name__ == "__main__":
 
     dev_filename, test_filename, word, out_filename = args
     vocabulary_size = 300000
+
+    articles_data = read_articles_file(dev_filename)
+    corpus = concat_articles(articles_data)
+
     outputs = [
         dev_filename,
         test_filename,
@@ -20,6 +47,7 @@ if __name__ == "__main__":
         out_filename,
         vocabulary_size,
         1 / vocabulary_size,
+        len(corpus.split()),
     ]
 
     with open(out_filename, "w") as f:
